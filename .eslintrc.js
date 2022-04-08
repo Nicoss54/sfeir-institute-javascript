c50037dae82c4fe17c262429249fa6e6c850157b
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2022-04-08 15:04:35 +0200

    feat(workshop): :tada: initialize nx workshop with variables exercice

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    semi: ['error', 'always'],
+    'max-lines': ['error', 120],
+    'object-curly-spacing': ['error', 'always'],
+    'array-bracket-spacing': ['error', 'never'],
+    'no-var': 0,
+    'prefer-const': 0,
+    'vars-on-top': 0,
+  },
+};
---
+++ apps/01-variables/main.js
@@ -0,0 +1,23 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
+
+/**
+ * TODO:
+ *
+ * Déclarer de trois façons différentes les variables suivantes:
+ *  - lastName
+ *  - firstName
+ *  - age
+ *
+ * Amusez vous avec vos variables pour constater les différentes propriétés que vous offres les différents mots clé de déclaration
+ *
+ * Affichez vos variables à l'aide de l'instruction console.log()
+ *
+ * Pensez bien à inspecter votre page en faisant click-droit/inspecter
+ *
+ * Rendez vous dans l'onglet console de l'inspection pour voir vos résultats
+ */
